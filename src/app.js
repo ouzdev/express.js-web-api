3128b25693838e3513623d967b0940413deef263
ouzdev <oguzcangencc@hotmail.com>
2022-03-20 20:37:56 +0300

    Some changes have made in app.js

--- src/app.js
+++ src/app.js
@@ -3,23 +3,18 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.get("/products", (req,res) => {
+app.get("/products",(req,res) => {
+
     const products = [
     {
-      id: 1,
-      name: "hammer",
+        id:1,
+        name:"Product 1"
     },
     {
-      id: 2,
-      name: "screwdriver",
-    },
-    ,
-    {
-      id: 3,
-      name: "wrench",
-    },
-   ];
-  
-   res.json(products);
-  });
+        id:2,
+        name:"Product 2"
+    }
+    ];
+    res.json(products);
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
