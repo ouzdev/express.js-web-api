d50cf54da347148a6e877a792efe0ac598ccf3e6
ouzdev <oguzcangencc@hotmail.com>
2022-03-21 20:19:22 +0300

    Coded a server side and a client side for middleware

---
+++ src/manage-a-request-lifecycle/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+function isAuthorized(req,res, next) {
+    const auth = req.headers.authorization;
+    if (auth === 'secretPassword') {
+      next();
+    } else {
+      res.status(401);
+      res.send('Not permitted');
+    }
+  }
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/users',isAuthorized, (req,res) => {
+  res.json([{
+    id: 1,
+    name: 'User Userson'
+  }])
+})
+
+app.get('/products', (req, res) => {
+  res.json([{
+    id: 1,
+    name: 'The Bluest Eye'
+  }])
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ src/manage-a-request-lifecycle/client.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+http.get({
+  port: 3000,
+  hostname: 'localhost',
+  path: '/users',
+  headers: {
+      authorization:'secretPassword'
+  }
+}, (res) => {
+  console.log("connected");
+  res.on("data", (chunk) => {
+    console.log("chunk", "" + chunk);
+  });
+  res.on("end", () => {
+    console.log("No more data");
+  });
+  res.on("close", () => {
+    console.log("Closing connection");
+  });
+});
